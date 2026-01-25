9f9bec99afd9bb04b9edd0029a5b5561a60243ed
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-01-26 06:34:17 +0700

    Update storage_boost.sh

--- storage_boost.sh
+++ storage_boost.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
-clear
-echo "[*] Storage Boost & Cache Cleaner"
-su -c "
-echo 3 > /proc/sys/vm/drop_caches
-sync
-rm -rf /data/local/tmp/*
-"
-echo "[✓] RAM & cache cleaned"
-df -h
-read -p "Enter untuk kembali"
-bash ../main.sh
+#!/system/bin/sh
+su -c "fstrim -v /data" >/dev/null
+echo "[✓] STORAGE TRIM"
+read -p "Enter..."
+exec ../main.sh
