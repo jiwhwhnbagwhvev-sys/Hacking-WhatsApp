0e712e6aea8054748c44a6c6227e41293fab697a
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-01-26 08:14:20 +0700

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -1,9 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "[*] System Info"
-getprop ro.product.model
-getprop ro.build.version.release
-getprop ro.product.cpu.abi
-uptime
+. ./modules/_common.sh
+
+MODUL="System Info"
+echo "[*] Menjalankan $MODUL"
+loading
+
+echo "Model: $(getprop ro.product.model)"
+echo "Android: $(getprop ro.build.version.release)"
+echo "CPU: $(getprop ro.product.cpu.abi)"
+echo "Uptime: $(uptime)"
 free -h
-read -p "Enter untuk kembali atau q untuk keluar: " c
-[[ "$c" =~ ^[qQ]$ ]] && exit || return
+
+back
