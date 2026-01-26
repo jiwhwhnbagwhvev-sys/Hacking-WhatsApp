de0fecb6344430a1e0e3e24dc0db5ae1e29f0d59
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-01-26 07:58:05 +0700

    Update root_check.sh

--- root_check.sh
+++ root_check.sh
@@ -1,10 +1,8 @@
+cat > modules/root_check.sh <<'EOF'
 #!/data/data/com.termux/files/usr/bin/bash
+. ./modules/_common.sh
 echo "[*] Root Check"
-if command -v su >/dev/null 2>&1; then
-    su -c "id" | grep -q uid=0 && echo "[✓] ROOT AKTIF" || echo "[✗] ROOT TIDAK TERDETEKSI"
-else
-    echo "[!] Tidak ada akses root"
-fi
-echo
-read -p "Tekan Enter untuk kembali ke menu..."
-exec ../main.sh
+need_root
+tsu -c "id"
+back
+EOF
