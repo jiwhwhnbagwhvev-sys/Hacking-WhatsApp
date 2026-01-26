e79d7262a831a4a8d2b1ed96d159cee85e554e52
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-01-26 07:35:58 +0700

    Update root_check.sh

--- root_check.sh
+++ root_check.sh
@@ -1,4 +1,10 @@
-#!/system/bin/sh
-su -c id | grep -q uid=0 && echo "[✓] ROOT AKTIF" || echo "[✗] ROOT OFF"
-read -p "Enter..."
+#!/data/data/com.termux/files/usr/bin/bash
+echo "[*] Root Check"
+if command -v su >/dev/null 2>&1; then
+    su -c "id" | grep -q uid=0 && echo "[✓] ROOT AKTIF" || echo "[✗] ROOT TIDAK TERDETEKSI"
+else
+    echo "[!] Tidak ada akses root"
+fi
+echo
+read -p "Tekan Enter untuk kembali ke menu..."
 exec ../main.sh
