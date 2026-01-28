f2fd1460fd40967712ce35c8688ede2fb86df07d
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-01-28 20:25:49 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -47,8 +47,8 @@ echo -e "${WHT}
 6 GPU Boost         14 SELinux Status   22 Kernel Info
 7 RAM Cleaner       15 SELinux Perm     23 Backup Apps
 8 Disable Thermal   16 Basic Root Hide  24 Network Speed & IP Check   
-25 WiFi Intruder Scanner   28 LED BLE Control Center            
-26 Auto Spam Block Guard.  
+25 WiFi Intruder Scanner   28 LED BLE Control Center   
+26 Auto Spam Block Guard.  29 network learning jaringan 
 27 HP Security Guard
 0 Exit.          
 ${R}"
@@ -89,6 +89,7 @@ while true; do
         26) bash modules/auto_spam_block_guard.sh ;;
         27) bash modules/hp_security_guard.sh ;;
         28) bash modules/led_ble_control.sh ;;
+        29) bash modules/network_learning.sh ;;
         0)  echo "[✓] Keluar..."; exit ;;
         *)  echo "[!] Pilihan salah"; sleep 1 ;;
     esac
