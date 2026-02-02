367d03f329a8f7f0a6f7ef169598485e5cc0737d
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-02-03 06:17:50 +0700

    Update login_loader.sh

--- login_loader.sh
+++ login_loader.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
-if [ ! -f login.sh ]; then
-  echo "[*] Unlocking login..."
-  gpg --quiet --batch --yes --decrypt login.sh.gpg > login.sh
-fi
+#!/data/data/com.termux/files/usr/bin/bash
 
-chmod +x login.sh
-./login.sh
+clear
+echo -e "\e[36m[ SYSTEM LOGIN ]\e[0m"
+sleep 1
+
+# decrypt sementara
+gpg --quiet --batch --yes -o /tmp/login_tmp.sh -d login.sh.gpg
+
+chmod +x /tmp/login_tmp.sh
+bash /tmp/login_tmp.sh
+
+# hapus lagi
+rm -f /tmp/login_tmp.sh
