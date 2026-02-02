049e9552e9a681f04ce3e43ed3e4b3bff7495706
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-02-03 06:29:43 +0700

    Update login_loader.sh

--- login_loader.sh
+++ login_loader.sh
@@ -4,11 +4,18 @@ clear
 echo -e "\e[36m[ SYSTEM LOGIN ]\e[0m"
 sleep 1
 
-# decrypt sementara
-gpg --quiet --batch --yes -o /tmp/login_tmp.sh -d login.sh.gpg
+TMP="$HOME/.login_tmp.sh"
 
-chmod +x /tmp/login_tmp.sh
-bash /tmp/login_tmp.sh
+# decrypt sementara ke HOME (bukan /tmp)
+gpg --quiet --batch --yes -o "$TMP" -d login.sh.gpg
+
+if [ ! -f "$TMP" ]; then
+    echo "Decryption failed!"
+    exit 1
+fi
+
+chmod +x "$TMP"
+bash "$TMP"
 
 # hapus lagi
-rm -f /tmp/login_tmp.sh
+rm -f "$TMP"
