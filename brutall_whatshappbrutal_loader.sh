4bc6cc779309dca01b7188f90f1f1dcb5d69c692
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-02-18 17:08:20 +0700

    Create brutall_whatshappbrutal_loader.sh

---
+++ brutall_whatshappbrutal_loader.sh
@@ -0,0 +1,30 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# Loader: decrypt & run main.c.gpg dengan password
+
+BRUTALL_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Minta password user
+read -sp "Masukkan passphrase untuk main.c.gpg: " PASS
+echo
+
+# Decrypt file sementara
+gpg --batch --yes --passphrase "$PASS" -o "$BRUTALL_DIR/main.c" -d "$BRUTALL_DIR/main.c.gpg"
+if [ $? -ne 0 ]; then
+    echo "[!] Passphrase salah atau dekripsi gagal!"
+    exit 1
+fi
+
+# Compile main.c
+gcc "$BRUTALL_DIR/main.c" -o "$BRUTALL_DIR/main"
+if [ $? -ne 0 ]; then
+    echo "[!] Compile gagal!"
+    exit 1
+fi
+
+# Jalankan program
+"$BRUTALL_DIR/main"
+
+# Hapus file sementara
+rm -f "$BRUTALL_DIR/main.c" "$BRUTALL_DIR/main"
+
+echo "[✓] Selesai menjalankan program."
