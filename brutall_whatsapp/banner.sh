c89f51e380b655dbcaac5d6e59e7f588f8db3b6b
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-02-19 19:12:14 +0700

    Create banner.sh

---
+++ brutall_whatsapp/banner.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+clear
+
+# warna
+green='\033[1;32m'
+yellow='\033[1;33m'
+purple='\033[1;35m'
+reset='\033[0m'
+
+echo -e "${purple}"
+echo "      |"
+echo "     / \\"
+echo "    /   \\"
+echo "   |  o  |"
+echo "   |  v  |"
+echo "   |  4  |"
+echo "  /|     |\\"
+echo " /_|_____|_\\"
+echo -e "${reset}"
+
+echo -e "${green}BRUTALWA${reset}"
+echo "----------------------------------"
+
+echo -e "${yellow}* Creator  : Sanz${reset}"
+echo -e "${yellow}* Youtube  : FREE TUTORIAL${reset}"
+echo -e "${yellow}* Channel  : Pecinta hpkentang${reset}"
+echo -e "${green}* Github   : github.com/Sxp-ID${reset}"
+echo -e "${green}* Support  : sociabuzz.com/sanzexde${reset}"
+
+echo ""
+echo -e "${yellow}Saya kerja sama dengan YouTuber FREE TUTORIAL${reset}"
+echo -e "${yellow}Channel : Pecinta hpkentang${reset}"
+echo -e "${yellow}Tunggu 5-10 menit untuk masuk ke halaman utama...${reset}"
+
+sleep 5
