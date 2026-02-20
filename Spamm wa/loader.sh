f7b44ea761abb0fb69e0d5257d752d678791a923
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-02-20 21:30:06 +0700

    Update loader.sh

--- Spamm wa/loader.sh
+++ Spamm wa/loader.sh
@@ -1,44 +1,49 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # Warna
-YELLOW='\033[1;33m'
 GREEN='\033[1;32m'
-RED='\033[1;31m'
+YELLOW='\033[1;33m'
 WHITE='\033[1;37m'
 CYAN='\033[1;36m'
 NC='\033[0m'
 
 clear
 
-# Ambil waktu & tanggal
-tanggal=$(date +"%d-%m-%Y")
-jam=$(date +"%H:%M:%S")
+# Buka Channel YouTube otomatis
+am start -a android.intent.action.VIEW -d "https://youtube.com/@pecinta-hpkentang?si=RAYC769vFGgRIDHX"
 
-echo -e "${WHITE}╔══════════════════════════════════════════════════════╗${NC}"
-echo -e "${RED}              LOGIN - PREMIUM${NC}"
-echo -e "${WHITE}                 versi 2.1.0${NC}"
-echo -e "${WHITE}     Login - Page Premium - Created Vindra${NC}"
-echo -e "${WHITE}╚══════════════════════════════════════════════════════╝${NC}"
+sleep 3
+clear
+
+# LOGO BESAR
+echo -e "${GREEN}"
+echo "██     ██ ██   ██  █████  ████████ ███████  ██████  ██████ "
+echo "██     ██ ██   ██ ██   ██    ██    ██      ██    ██ ██   ██"
+echo "██  █  ██ ███████ ███████    ██    █████   ██    ██ ██████ "
+echo "██ ███ ██ ██   ██ ██   ██    ██    ██      ██    ██ ██   ██"
+echo " ███ ███  ██   ██ ██   ██    ██    ███████  ██████  ██   ██"
+echo -e "${WHITE}"
+echo "              WHATSAPP TOOLS v2.0"
+echo "---------------------------------------------"
+echo -e "${NC}"
+
+echo -e "${YELLOW}* Creator   : hpkentang${NC}"
+echo -e "${YELLOW}* Youtube   : Pecinta hpkentang${NC}"
 echo ""
 
-echo -e "${CYAN}Tanggal : $tanggal${NC}"
-echo -e "${CYAN}Jam     : $jam${NC}"
+echo -e "${YELLOW}Pakai dengan bijak jangan di salah gunakan${NC}"
+echo -e "${YELLOW}Resiko di tanggung pengguna${NC}"
 echo ""
 
-echo -e "${YELLOW}⚠ Pakai dengan bijak jangan di salah gunakan${NC}"
-echo -e "${YELLOW}⚠ Resiko di tanggung pengguna${NC}"
-echo -e "${YELLOW}⚠ by Pecinta hpkentang${NC}"
-echo ""
-echo -e "${GREEN}Tunggu 1 sampai 5 menit mohon bersabar...${NC}"
-echo -e "${GREEN}Untuk menjaga keamanan sistem...${NC}"
+echo -e "${CYAN}Tunggu 5-10 menit untuk masuk ke halaman utama...${NC}"
 echo ""
 
-# Animasi loading 5 detik
+# Animasi loading
 for i in {1..5}
 do
-    echo -e "${WHITE}Loading keamanan [$i/5]...${NC}"
-    sleep 1
+  echo -e "${WHITE}Loading [$i/5]...${NC}"
+  sleep 1
 done
 
 sleep 1
-bash login.sh
+bash menu.sh
