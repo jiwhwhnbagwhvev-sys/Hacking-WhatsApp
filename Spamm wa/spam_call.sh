18d25cae891ef51d7ea800c0ca6d4aa7a90a53f4
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-03-01 06:22:50 +0700

    Update spam_call.sh

--- Spamm wa/spam_call.sh
+++ Spamm wa/spam_call.sh
@@ -1,14 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # ==========================================
-# Spamm call WhatsApp 
+# Spam Call WhatsApp 
 # ==========================================
 
-RED='\033[1;31m'
 GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
 CYAN='\033[1;36m'
 PINK='\033[1;95m'
 WHITE='\033[1;37m'
+RED='\033[1;31m'
 NC='\033[0m'
 
 ADMIN="6285283786794"
@@ -39,8 +40,8 @@ echo -e "${RED}Nomor tidak boleh kosong!${NC}"
 exit
 fi
 
-# format ke 62
-nomor="62$input"
+# nomor tanpa 62
+nomor="$input"
 
 echo ""
 echo -e "${CYAN}Menghubungkan ke server WhatsApp...${NC}"
@@ -57,8 +58,9 @@ done
 echo ""
 sleep 1
 
-# kirim ke WhatsApp admin
-TEXT="Menu Spam Call Fitur 1 | Target:$nomor"
+# pesan WhatsApp RAPAT TANPA JARAK
+TEXT="Menu Spamm Call Fitur 1 | Nomor Target: $nomor | Status: Aktif ✓"
+
 ENCODE=$(echo "$TEXT" | sed 's/ /%20/g')
 
 echo -e "${CYAN}Membuka WhatsApp Admin...${NC}"
@@ -66,16 +68,15 @@ echo -e "${CYAN}Membuka WhatsApp Admin...${NC}"
 am start -a android.intent.action.VIEW \
 -d "https://wa.me/$ADMIN?text=$ENCODE"
 
-# tunggu kirim pesan
-sleep 7
+sleep 6
 
 logo
 
-echo -e "${WHITE}Menu   : Spamm Call Fitur 1${NC}"
+echo -e "${WHITE}Menu   : Spamm Call WhatsApp${NC}"
 echo -e "${WHITE}Target : $nomor${NC}"
 echo ""
 
-echo -e "${GREEN}Engine Aktif...${NC}"
+echo -e "${GREEN}Memulai Spam Engine...${NC}"
 echo ""
 
 count=1
@@ -83,17 +84,15 @@ count=1
 while true
 do
 
-case $((count % 5)) in
-0) color=$RED ;;
-1) color=$GREEN ;;
-2) color=$CYAN ;;
-3) color=$PINK ;;
-4) color=$WHITE ;;
-esac
+if ((count % 2 == 0)); then
+color=$GREEN
+else
+color=$YELLOW
+fi
 
-printf "${color}[%d] Mencoba Spamm call kenomor %s ...${NC}\n" "$count" "$nomor"
+printf "${color}[%d] mencoba spamm call ke nomor %s ...${NC}\n" "$count" "$nomor"
 
-sleep 0.05
+sleep 0.08
 
 count=$((count+1))
 
