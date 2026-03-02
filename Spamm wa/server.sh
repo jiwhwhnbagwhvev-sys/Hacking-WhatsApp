2bd8e11d2810ddfe38a6a42a2b7c925b61ee2f05
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-03-03 05:31:01 +0700

    Create server.sh

---
+++ Spamm wa/server.sh
@@ -0,0 +1,69 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+echo "[INFO] Harap Tunggu Sedang Menjalankan Program"
+echo "[INFO] Security : Pyramid v9 Beta Plugin Pyramid Dom Network v10"
+echo ""
+
+cat << "EOF"
+
+██████╗ ██████╗ ██╗██╗   ██╗ █████╗ ████████╗███████╗    ██████╗  ██████╗ ██╗     ██╗ ██████╗ ███████╗
+██╔══██╗██╔══██╗██║██║   ██║██╔══██╗╚══██╔══╝██╔════╝    ██╔══██╗██╔═══██╗██║     ██║██╔════╝ ██╔════╝
+██████╔╝██████╔╝██║██║   ██║███████║   ██║   █████╗      ██████╔╝██║   ██║██║     ██║██║  ███╗█████╗  
+██╔═══╝ ██╔══██╗██║╚██╗ ██╔╝██╔══██║   ██║   ██╔══╝      ██╔═══╝ ██║   ██║██║     ██║██║   ██║██╔══╝  
+██║     ██║  ██║██║ ╚████╔╝ ██║  ██║   ██║   ███████╗    ██║     ╚██████╔╝███████╗██║╚██████╔╝███████╗
+╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝  ╚═╝  ╚═╝   ╚═╝   ╚══════╝    ╚═╝      ╚═════╝ ╚══════╝╚═╝ ╚═════╝ ╚══════╝
+
+EOF
+
+echo ""
+echo "=============================================================="
+echo "                 PRIVATE POLICE v3.1"
+echo "=============================================================="
+echo "Fitur ini sudah di update"
+echo "Diperbarui versi 3.1 pada tgl 3 Maret 2026"
+echo "[ Perjanjian konsument terhadap developer ]
+dengan ini saya menyetujui untuk memtuhi aturan dari creato r atau developer
+dengan menggunakan nya sesuai posisi sebagai user atau kons ument
+saya tidak akan melakukan percobaan crack, reverse, deobfus
+cate atau sejenis nya yang akan
+merugikan developer, jika ini saya langgar maka saya siap m enerima semua resiko dari mekanisme pyramid encient killer.
+DAN SAYA TIDAK AKAN MENYALAHKAN DEVELOPER DI KARENAKAN INI KARENA KESERAKAHAN
+DAN TINDAKAN SAYA YANG LANCANG MAKA SAYA SIAP MENERIMA APAP UN RESIKO NYA DAN TIDAK AKAN MENUNTUT
+echo"
+
+read -p "[enter]"
+
+# ==============================
+# SETUP
+# ==============================
+
+clear
+echo "[?] Melakukan setup 1"
+sleep 3
+
+# ==============================
+# KOMPILASI
+# ==============================
+
+clear
+echo "[?] Memulai kompilasi mohon tunggu.."
+sleep 2
+
+echo ""
+echo "[$(date)] Mengecek status Mencurigakan ... (Security v10)"
+sleep 2
+
+echo "[$(date)] Proses biasanya memakan waktu 5-10 menit mohon untuk bersabar demi keamanan"
+sleep 2
+
+echo ""
+echo "[>] Menjalankan script...."
+sleep 2
+
+# ==============================
+# LANJUT KE SCRIPT UTAMA
+# ==============================
+
+# ganti main.sh dengan file utama kamu
+bash banner.sh
