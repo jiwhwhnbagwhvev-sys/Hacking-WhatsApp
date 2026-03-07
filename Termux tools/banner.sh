2ce9ca25938cf9a0e0ee174795c78498b3c9226b
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-07 12:00:33 +0700

    Create banner.sh

---
+++ Termux tools/banner.sh
@@ -0,0 +1,36 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+
+red='\033[1;31m'
+white='\033[1;37m'
+black='\033[0;30m'
+green='\033[1;32m'
+reset='\033[0m'
+
+echo -e "$red"
+echo "              █████████████"
+echo "           ███           ███"
+echo "         ██   ☠       ☠    ██"
+echo "        ██       ▄▄▄        ██"
+echo "        ██      █████       ██"
+echo "         ██               ██"
+echo "           ███         ███"
+echo "              █████████"
+echo -e "$reset"
+
+echo -e "$black"
+echo "       ▄██████▄     ▄██████▄"
+echo "======|  ████  |===|  ████  |======"
+echo "       ▀██████▀     ▀██████▀"
+echo -e "$reset"
+
+echo -e "$white"
+echo "===================================="
+echo "        Yt: pecinta hpkentang kentang"
+echo "===================================="
+echo -e "$reset"
+
+echo -e "$green"
+echo "        ☠  TERMUX DARK SYSTEM  ☠"
+echo -e "$reset"
