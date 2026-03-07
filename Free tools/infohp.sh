a5bfd1511286be39aefc32fdabc905d32bbc8007
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-07 17:58:24 +0700

    Create infohp.sh

---
+++ Free tools/infohp.sh
@@ -0,0 +1,11 @@
+clear
+
+echo "================================="
+echo "        INFO HP ANDROID"
+echo "================================="
+echo ""
+
+neofetch
+
+echo ""
+read -p "Tekan ENTER untuk keluar..."
