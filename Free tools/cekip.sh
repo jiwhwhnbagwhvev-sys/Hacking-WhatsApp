23c8f51b04613a96d9ca0cefe59e7c18696d2959
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-07 17:57:38 +0700

    Create cekip.sh

---
+++ Free tools/cekip.sh
@@ -0,0 +1,13 @@
+clear
+
+echo "================================="
+echo "         CEK IP ADDRESS"
+echo "================================="
+echo ""
+
+echo "IP KAMU :"
+curl ifconfig.me
+
+echo ""
+echo ""
+read -p "Tekan ENTER untuk keluar..."
