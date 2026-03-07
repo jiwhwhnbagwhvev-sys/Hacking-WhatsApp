72d4d90d59bf4f40c20251c3e2d2979e1508f180
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-07 18:08:23 +0700

    Create jaringan.sh

---
+++ Free tools/jaringan.sh
@@ -0,0 +1,12 @@
+clear
+
+echo "================================="
+echo "        INFO JARINGAN"
+echo "================================="
+echo ""
+
+ip a
+
+echo ""
+read -p "Tekan ENTER untuk kembali..."
+bash menu.sh
