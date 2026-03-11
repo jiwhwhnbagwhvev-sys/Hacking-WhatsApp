f3483c6c169508a11468f8641ba329c56acadeaf
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:37:00 +0700

    Update and rename main.sh to main.sh

---
+++ PecintaHPKentang/main.sh
@@ -0,0 +1,29 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+
+# STARTUP
+bash ~/PecintaHPKentang/scripts/hacker_startup.sh
+
+# LOADING
+bash ~/PecintaHPKentang/scripts/loading.sh
+bash ~/PecintaHPKentang/scripts/progress.sh
+bash ~/PecintaHPKentang/scripts/scan.sh
+
+# VISUAL
+bash ~/PecintaHPKentang/assets/crown.sh
+bash ~/PecintaHPKentang/assets/skull_pro.sh
+
+# PANEL
+bash ~/PecintaHPKentang/ui/glow.sh
+bash ~/PecintaHPKentang/ui/system_panel.sh
+
+# SYSTEM INFO
+bash ~/PecintaHPKentang/modules/battery.sh
+bash ~/PecintaHPKentang/modules/ram.sh
+bash ~/PecintaHPKentang/modules/cpu.sh
+
+# EXTRA
+bash ~/PecintaHPKentang/scripts/matrix.sh
+bash ~/PecintaHPKentang/ui/menu.sh
+bash ~/PecintaHPKentang/ui/footer.sh
