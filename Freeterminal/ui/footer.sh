bc09e6ab6503f948c517bf3fa4fcb3e921cc406d
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:16:53 +0700

    Create footer.sh

---
+++ Freeterminal/ui/footer.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+green='\033[1;32m'
+yellow='\033[1;33m'
+white='\033[1;37m'
+reset='\033[0m'
+
+echo ""
+echo -e "$green━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "$white   TERMINAL READY - PECINTA HP KENTANG"
+echo -e "$yellow   Welcome To Hacker Style Termux"
+echo -e "$green━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "$reset"
