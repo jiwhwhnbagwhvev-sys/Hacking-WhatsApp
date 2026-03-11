d6933cbd71f32ae213866ae8689de5dc65d3b6cb
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:34:09 +0700

    Update main.sh

--- Freeterminal/main.sh
+++ Freeterminal/main.sh
@@ -3,27 +3,27 @@
 clear
 
 # STARTUP
-bash ~/PecintaHPKentang/scripts/hacker_startup.sh
+bash scripts/hacker_startup.sh
 
 # LOADING
-bash ~/PecintaHPKentang/scripts/loading.sh
-bash ~/PecintaHPKentang/scripts/progress.sh
-bash ~/PecintaHPKentang/scripts/scan.sh
+bash scripts/loading.sh
+bash scripts/progress.sh
+bash scripts/scan.sh
 
 # VISUAL
-bash ~/PecintaHPKentang/assets/crown.sh
-bash ~/PecintaHPKentang/assets/skull_pro.sh
+bash assets/crown.sh
+bash assets/skull_pro.sh
 
 # PANEL
-bash ~/PecintaHPKentang/ui/glow.sh
-bash ~/PecintaHPKentang/ui/system_panel.sh
+bash ui/glow.sh
+bash ui/system_panel.sh
 
 # SYSTEM INFO
-bash ~/PecintaHPKentang/modules/battery.sh
-bash ~/PecintaHPKentang/modules/ram.sh
-bash ~/PecintaHPKentang/modules/cpu.sh
+bash modules/battery.sh
+bash modules/ram.sh
+bash modules/cpu.sh
 
 # EXTRA
-bash ~/PecintaHPKentang/scripts/matrix.sh
-bash ~/PecintaHPKentang/ui/menu.sh
-bash ~/PecintaHPKentang/ui/footer.sh
+bash scripts/matrix.sh
+bash ui/menu.sh
+bash ui/footer.sh
