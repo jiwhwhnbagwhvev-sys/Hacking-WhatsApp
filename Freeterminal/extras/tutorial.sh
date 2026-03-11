91c9747b18a51682b2dc1df950a903adbc5841e4
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:02:29 +0700

    Create tutorial.sh

---
+++ Freeterminal/extras/tutorial.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+red='\033[1;31m'
+white='\033[1;37m'
+yellow='\033[1;33m'
+reset='\033[0m'
+
+echo ""
+
+echo -e "$red▶ $yellow FREE TUTORIAL $reset"
+
+echo -e "$white YouTube : Pecinta HP Kentang $reset"
+
+echo ""
