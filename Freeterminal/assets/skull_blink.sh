303eea9a0d0fa77e627b05d215051bde8ea77d15
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:22:01 +0700

    Create skull_blink.sh

---
+++ Freeterminal/assets/skull_blink.sh
@@ -0,0 +1,41 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+red='\033[1;31m'
+white='\033[1;37m'
+yellow='\033[1;33m'
+reset='\033[0m'
+
+while true
+do
+
+clear
+echo -e "$red      👑"
+echo -e "$white"
+echo "      ● ● ● ● ● ● ●"
+echo "    ● ● ● ● ● ● ● ● ●"
+echo "   ● ●   ● ● ● ●   ● ●"
+echo -e "  ● ●    $red●   ●$white    ● ●"
+echo "  ● ●             ● ●"
+echo "   ● ●           ● ●"
+echo "    ● ● ● ● ● ● ●"
+echo -e "$yellow     ● ● ● ●"
+echo -e "$reset"
+
+sleep 0.5
+
+clear
+echo -e "$red      👑"
+echo -e "$white"
+echo "      ● ● ● ● ● ● ●"
+echo "    ● ● ● ● ● ● ● ● ●"
+echo "   ● ●   ● ● ● ●   ● ●"
+echo -e "  ● ●    $white○   ○$white    ● ●"
+echo "  ● ●             ● ●"
+echo "   ● ●           ● ●"
+echo "    ● ● ● ● ● ● ●"
+echo -e "$yellow     ● ● ● ●"
+echo -e "$reset"
+
+sleep 0.5
+
+done
