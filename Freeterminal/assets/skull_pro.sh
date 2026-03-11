a3447a13877682475e8d203d021e7bc62501d8d2
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:15:14 +0700

    Create skull_pro.sh

---
+++ Freeterminal/assets/skull_pro.sh
@@ -0,0 +1,35 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+red='\033[1;31m'
+white='\033[1;37m'
+yellow='\033[1;33m'
+reset='\033[0m'
+
+echo -e "$red"
+echo "                 👑"
+echo -e "$white"
+
+echo "        ● ● ● ● ● ● ● ● ● ●"
+echo "     ● ● ● ● ● ● ● ● ● ● ● ●"
+echo "    ● ●    ● ● ● ● ● ●    ● ●"
+echo "   ● ●      ● ● ● ●      ● ●"
+echo "   ● ●      ● ● ● ●      ● ●"
+echo "    ● ●    ● ● ● ● ● ●    ● ●"
+echo "     ● ● ● ● ● ● ● ● ● ● ●"
+echo "        ● ● ● ● ● ● ● ●"
+
+echo -e "$red"
+echo "          ● ●     ● ●"
+
+echo -e "$yellow"
+
+echo "        ● ● ● ● ● ● ● ● ● ●"
+echo "     ● ● ● ● ● ● ● ● ● ● ● ●"
+echo "    ● ●    ● ● ● ● ● ●    ● ●"
+echo "   ● ●      ● ● ● ●      ● ●"
+echo "   ● ●      ● ● ● ●      ● ●"
+echo "    ● ●    ● ● ● ● ● ●    ● ●"
+echo "     ● ● ● ● ● ● ● ● ● ● ●"
+echo "        ● ● ● ● ● ● ●"
+
+echo -e "$reset"
