3390959152da9cf1dbdae447ee906dc54eadbb35
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:00:09 +0700

    Create frame.sh

---
+++ Freeterminal/ui/frame.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+white='\033[1;37m'
+reset='\033[0m'
+
+echo -e "$white"
+echo "╭──────────────────────────────────╮"
+echo "│                                  │"
+echo "│          SYSTEM PANEL            │"
+echo "│                                  │"
+echo "╰──────────────────────────────────╯"
+echo -e "$reset"
