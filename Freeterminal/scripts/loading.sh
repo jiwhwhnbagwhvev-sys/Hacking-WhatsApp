e2f5a0983f9bfb228e591f6d2d6b98772d2e39f7
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:03:12 +0700

    Create loading.sh

---
+++ Freeterminal/scripts/loading.sh
@@ -0,0 +1,25 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+green='\033[1;32m'
+yellow='\033[1;33m'
+reset='\033[0m'
+
+echo -e "$green"
+echo "Initializing System..."
+sleep 0.5
+
+echo "Loading Modules..."
+sleep 0.5
+
+echo "Checking Device..."
+sleep 0.5
+
+echo "Starting Interface..."
+sleep 0.5
+
+echo -e "$yellow"
+echo "SYSTEM READY"
+echo -e "$reset"
+
+sleep 1
+clear
