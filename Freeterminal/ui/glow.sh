cabda868f5d020a175c2e08bb17d6befac15ee4c
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:16:14 +0700

    Create glow.sh

---
+++ Freeterminal/ui/glow.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+yellow='\033[1;33m'
+white='\033[1;37m'
+red='\033[1;31m'
+reset='\033[0m'
+
+echo -e "$yellow"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "$white        PECINTA HP KENTANG TERMINAL"
+echo -e "$yellow━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "$reset"
+
+echo -e "$red  SYSTEM STATUS ACTIVE"
+echo -e "$reset"
