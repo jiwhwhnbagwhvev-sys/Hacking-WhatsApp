56b4dfbca3e73e9ec655c4ba963ef68428eb08a9
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:10:39 +0700

    Create skull_anim.sh

---
+++ Freeterminal/assets/skull_anim.sh
@@ -0,0 +1,57 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+red='\033[1;31m'
+white='\033[1;37m'
+yellow='\033[1;33m'
+reset='\033[0m'
+
+clear
+
+# frame 1
+echo -e "$red"
+echo "                👑"
+echo -e "$white"
+echo "        ● ● ● ● ● ● ● ● ●"
+echo "     ● ● ● ● ● ● ● ● ● ● ●"
+echo "    ● ●    ● ● ● ●    ● ●"
+echo "   ● ●      ● ●      ● ●"
+echo "   ● ●      ● ●      ● ●"
+echo "    ● ●    ● ● ● ●    ● ●"
+echo "     ● ● ● ● ● ● ● ● ●"
+echo "        ● ● ● ● ● ●"
+echo -e "$yellow"
+echo "        ● ● ● ● ● ● ●"
+echo "     ● ● ● ● ● ● ● ● ●"
+echo "    ● ●    ● ● ● ●    ● ●"
+echo "   ● ●      ● ●      ● ●"
+echo "   ● ●      ● ●      ● ●"
+echo "    ● ●    ● ● ● ●    ● ●"
+echo "     ● ● ● ● ● ● ● ● ●"
+echo "        ● ● ● ● ●"
+sleep 0.4
+
+clear
+
+# frame 2 (mata merah)
+echo -e "$red"
+echo "                👑"
+echo -e "$white"
+echo "        ● ● ● ● ● ● ● ● ●"
+echo "     ● ● ● ● ● ● ● ● ● ● ●"
+echo "    ● ●    ● ● ● ●    ● ●"
+echo "   ● ●      $red● ●$white      ● ●"
+echo "   ● ●      $red● ●$white      ● ●"
+echo "    ● ●    ● ● ● ●    ● ●"
+echo "     ● ● ● ● ● ● ● ● ●"
+echo "        ● ● ● ● ● ●"
+echo -e "$yellow"
+echo "        ● ● ● ● ● ● ●"
+echo "     ● ● ● ● ● ● ● ● ●"
+echo "    ● ●    ● ● ● ●    ● ●"
+echo "   ● ●      ● ●      ● ●"
+echo "   ● ●      ● ●      ● ●"
+echo "    ● ●    ● ● ● ●    ● ●"
+echo "     ● ● ● ● ● ● ● ● ●"
+echo "        ● ● ● ● ●"
+
+echo -e "$reset"
