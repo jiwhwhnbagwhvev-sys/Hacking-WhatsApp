ab1c0ac623e4f93a01a4674bc2e87b304fdf7c99
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 09:58:36 +0700

    Create skull.sh

---
+++ Freeterminal/assets/skull.sh
@@ -0,0 +1,35 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+white='\033[1;37m'
+yellow='\033[1;33m'
+red='\033[1;31m'
+reset='\033[0m'
+
+echo -e "$white"
+
+echo "      • • • • • • • • • • • •"
+echo "   • • • • • • • • • • • • • •"
+echo "  • •    • • • • • •    • •"
+echo " • •      • • • •      • •"
+echo " • •      • • • •      • •"
+echo "  • •    • • • • • •    • •"
+
+echo -e "$red"
+echo "           • • •"
+echo -e "$white"
+
+echo "  • • • • • • • • • • • •"
+echo "      • • • • • • •"
+
+echo -e "$yellow"
+
+echo "      • • • • • • • • • •"
+echo "   • • • • • • • • • • • • •"
+echo "  • •    • • • • • •    • •"
+echo " • •      • • • •      • •"
+echo " • •      • • • •      • •"
+echo "  • •    • • • • • •    • •"
+echo "   • • • • • • • • • • • •"
+echo "      • • • • • • •"
+
+echo -e "$reset"
