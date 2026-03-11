31f010a76e4badc4ffde07f5e354bcc340641dc0
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-11 10:22:41 +0700

    Create crown.sh

---
+++ Freeterminal/assets/crown.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+red='\033[1;31m'
+yellow='\033[1;33m'
+reset='\033[0m'
+
+echo -e "$red"
+echo "        /\        /\        /\ "
+echo "       /  \______/  \______/  \ "
+echo "      /                        \ "
+echo -e "$yellow     ██████████████████████"
+echo -e "$red        KING TERMINAL"
+echo -e "$reset"
