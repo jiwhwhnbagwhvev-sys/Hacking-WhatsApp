e07f5f004c7deec05bcb016e98b13656b546e4a5
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-12 22:28:39 +0700

    Update banner.sh

--- Spamm wa/banner.sh
+++ Spamm wa/banner.sh
@@ -13,36 +13,6 @@ NC='\033[0m'
 
 clear
 
-# ==============================
-#        TIME LOCK SYSTEM
-# ==============================
-
-current_hour=$(date +"%H")
-
-# Jika sebelum jam 04:00
-if [ "$current_hour" -lt 4 ]; then
-    echo ""
-    echo "======================================"
-    echo "   FITUR BELUM BISA DIGUNAKAN"
-    echo "   Tool online mulai jam 04:00"
-    echo "   Silakan tunggu sampai subuh."
-    echo "======================================"
-    echo ""
-    exit 1
-fi
-
-# Jika setelah jam 19:00
-if [ "$current_hour" -ge 19 ]; then
-    echo ""
-    echo "======================================"
-    echo "   ERROR : Fitur sedang offline"
-    echo "   Waktu penggunaan hanya sampai 19:00"
-    echo "   Silakan gunakan kembali besok."
-    echo "======================================"
-    echo ""
-    exit 1
-fi
-
 # ===== OPEN YOUTUBE =====
 echo -e "${CYAN}[•] Pecinta hpkentang...${NC}"
 sleep 1
