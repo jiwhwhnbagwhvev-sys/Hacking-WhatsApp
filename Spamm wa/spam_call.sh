915ecbf10d8f4bb3c3d1711ce9eb6c8037c7aa65
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-18 17:09:06 +0700

    Update spam_call.sh

--- Spamm wa/spam_call.sh
+++ Spamm wa/spam_call.sh
@@ -244,7 +244,7 @@ echo ""
 
 operator=$(deteksi_operator "$nomor")
 
-echo -e "${RED}•${WHITE} Carrier : ${GREEN}$operator${NC}"
+echo -e "${RED}•${WHITE} Carrier : ${YELLOW}$operator${NC}"
 echo -e "${RED}•${WHITE} Waktu : ${YELLOW}$salam${NC}"
 echo -e "${RED}•${WHITE} psn admin : ${RED}jangan pakai lebih dari 1 sesi ${YELLOW}:v${NC}"
 echo -e "${RED}•${WHITE} psn admin : ${YELLOW}Tunggu 5-10 menit ya ka baru masuk spamm nya ${RED}:v${NC}"
