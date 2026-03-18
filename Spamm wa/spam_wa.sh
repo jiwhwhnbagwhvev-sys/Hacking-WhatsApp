2a6688a07db5de200b4653a13285d408cd8e99e8
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-03-18 11:15:32 +0700

    Update spam_wa.sh

--- Spamm wa/spam_wa.sh
+++ Spamm wa/spam_wa.sh
@@ -232,6 +232,7 @@ echo ""
 operator=$(deteksi_operator "$nomor")
 
 echo -e "${RED}•${WHITE} Carrier : ${GREEN}$operator${NC}"
+echo -e "${RED}•${WHITE} Waktu : ${GREEN}$salam${NC}"
 echo -e "${RED}•${WHITE} psn admin : ${RED}jangan pakai lebih dari 1 sesi ${YELLOW}:v${NC}"
 echo -e "${RED}•${WHITE} note : ${RED}Exit ${YELLOW}--> ${RED}Ctrl z${NC}"
 echo ""
