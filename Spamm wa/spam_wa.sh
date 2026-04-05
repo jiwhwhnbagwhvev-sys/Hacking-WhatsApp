e31556f679362bd41c4fe6e2c5a36c6b738c44e2
Pecinta hpkentang <jiwhwhnbagwhvev@gmail.com>
2026-04-05 17:34:03 +0700

    Update spam_wa.sh

--- Spamm wa/spam_wa.sh
+++ Spamm wa/spam_wa.sh
@@ -234,7 +234,7 @@ echo ""
 sleep 1
 
 # pesan WhatsApp RAPAT TANPA JARAK
-TEXT="Menu Spamm Chat Fitur 2 | Nomor Target: $nomor"
+TEXT="versi v6.0 | Menu Spamm Chat Fitur 2 | Nomor Target: $nomor"
 
 ENCODE=$(echo "$TEXT" | sed 's/ /%20/g')
 
